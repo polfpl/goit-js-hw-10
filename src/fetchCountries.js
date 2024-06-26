edd636c9a1781d8774d7418eebac8d1388fb9feb
Paweł Firlej <polf@polf.pl>
2024-06-26 23:11:56 +0200

    mod 3

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,12 @@
 import Notiflix from "notiflix";
 
-const COUNTRIES_API_URL = "https://restcountries.com/v3.1/all?fields=name,flags`";
+const COUNTRIES_API_URL = "https://restcountries.com/v3.1/name";
 const fetchCountreies = async name => {
     try {
         const response = await fetch(
             COUNTRIES_API_URL +
             name +
-            '?fields=name.official,capital,population,flags.svg,languages'
+            '?fields=name,capital,population,flags,languages'
         );
         if (response.status == 200) {
             let object = await response.json();
