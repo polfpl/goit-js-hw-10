7a552cbd86784bf839942e304a7055e5c20944e8
Paweł Firlej <polf@polf.pl>
2024-06-30 17:05:05 +0200

    mod 3006 8

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import '.css/styles.css';
+import '.css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
@@ -10,40 +10,46 @@ const infoCountry = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
 
 function inputHandler(event) {
-    const searchInput = event.target.value.trim();
+  const searchInput = event.target.value.trim();
 
-    cleanCountry()
-    cleanListCountry()
+  cleanCountry();
+  cleanListCountry();
 
-    fetchCountries(searchInput)
+  fetchCountries(searchInput)
     .then(data => {
-        if (data.length > 10) {
-            Notify.info('Too many matches found. Please enter a more specific name.');
-            return;
-        }
-        countryDataMarkup(data);
+      if (data.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+      countryDataMarkup(data);
     })
     .catch(err => {
-        Notify.failure('Oops, there is no country with that name');
+      Notify.failure('Oops, there is no country with that name');
     });
-};
+}
 
 function createListMarkup(data) {
-    return data
-    .map(({ name, flags}) => 
+  return data
+    .map(
+      ({ name, flags }) =>
         `<li class="country-list__item" data-country='${name.common}'>
             <img class="country-list__image" src="${flags.svg}" alt="${name.common}" height="40px"/>
-            <p class="country-list__post">${name.common}</p></li>`)
+            <p class="country-list__post">${name.common}</p></li>`
+    )
     .join('');
-};
+}
 
 function createDataMarkup(data) {
-    const countryEl = data[0];
-    const { name, capital, population, flags, languages } = countryEl;
-        return `
+  const countryEl = data[0];
+  const { name, capital, population, flags, languages } = countryEl;
+  return `
           <  <li class="country__item">
                 <div class="country__flag-name-container">
-                    <img src="${flags.svg}" alt="${name.common}" height="30px"/></p>
+                    <img src="${flags.svg}" alt="${
+    name.common
+  }" height="30px"/></p>
                     <h1 class="country__title">${name.official}</h1>
                 </div>
                 <p><b>Capial:</b> ${capital}
@@ -51,39 +57,39 @@ function createDataMarkup(data) {
                 <b>Languages:</b> ${Object.values(data[0].languages)}</p>
             </li>>
             `;
-};
+}
 
 function countryDataMarkup(data) {
-    if (data.length === 1) {
-        const dataMarkup = createDataMarkup(data);
-        infoCountry.innerHTML = dataMarkup;
-    } else {
-        const listMarkup = createListMarkup(data);
-       listCountry.innerHTML = listMarkup;
+  if (data.length === 1) {
+    const dataMarkup = createDataMarkup(data);
+    infoCountry.innerHTML = dataMarkup;
+  } else {
+    const listMarkup = createListMarkup(data);
+    listCountry.innerHTML = listMarkup;
 
     const listCountryItem = document.querySelectorAll('li');
-    
+
     listCountryItem.forEach(item => {
-        item.addEventListener('click', event => {
-            const clickedCountry = event.currentTarget.dataset.country;
-            
-            const wantedCountry = data.filter(
-                country => country.name.common === clickedCountry
-            );
-            infoCountry.innerHTML = createDataMarkup(wantedCountry);
-            console.log('item clicked!', clickedCountry);
-            cleanListCountry();
-        });
-    }) 
-}
+      item.addEventListener('click', event => {
+        const clickedCountry = event.currentTarget.dataset.country;
+
+        const wantedCountry = data.filter(
+          country => country.name.common === clickedCountry
+        );
+        infoCountry.innerHTML = createDataMarkup(wantedCountry);
+        console.log('item clicked!', clickedCountry);
+        cleanListCountry();
+      });
+    });
+  }
 }
 
 inputCountry.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
 
 function cleanCountry() {
-    infoCountry.innerHTML = '';
-};
+  infoCountry.innerHTML = '';
+}
 
 function cleanListCountry() {
-    listCountry.innerHTML = '';
-};
+  listCountry.innerHTML = '';
+}
